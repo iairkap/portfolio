5788d1bd88a8ffeed52fd2a6c93af6ee4de8fb2d
iair kaplun <iairkap@gmail.com>
2024-02-15 12:56:49 -0300

    push-updates

--- src/app/portfolio/webProjects.js
+++ src/app/portfolio/webProjects.js
@@ -3,9 +3,133 @@ import realcoverb from "../../../public/linkedin pub.png";
 
 export const proyectosWebs = [
   {
+    name: "Weddinvitation",
+    backgroundContainer: "#818369",
+    textExplainEn: `WORK IN PROGRRESS: 
+    
+    Weddinvitation is a platform that allows couples to create and customize their wedding invitations. The platform offers a variety of templates, allowing couples to personalize their invitations to match their wedding theme. The platform also provides a guest list feature, allowing couples to trak their guest list and send invitations directly from the platform. The platform is built using Next.js, Prisma, and PostgreSQL, ensuring a seamless and secure user experience.
+    
+    Also we develop custom invitations according to the specific needs of the couple.
+
+    The upcoming features include : a split payment system, a split roles system, and a calendar of activities by guest roles.`,
+
+    textExplainEs: `EN PROGRESO:
+
+    Weddinvitation es una plataforma que permite a las parejas crear y personalizar sus invitaciones de boda. La plataforma ofrece una variedad de plantillas, permitiendo a las parejas personalizar sus invitaciones para que coincidan con el tema de su boda. La plataforma también ofrece una función de lista de invitados, que permite a las parejas hacer un seguimiento de su lista de invitados y enviar invitaciones directamente desde la plataforma. La plataforma está construida utilizando Next.js, Prisma y PostgreSQL, asegurando una experiencia de usuario fluida y segura.
+
+    También desarrollamos invitaciones personalizadas de acuerdo a las necesidades específicas de la pareja.
+
+    Las próximas características incluyen: un sistema de pago dividido, un sistema de roles divididos y un calendario de actividades por roles de invitados.`,
+
+    picture: `https://firebasestorage.googleapis.com/v0/b/real-cover.appspot.com/o/Screenshot%202024-02-15%20at%2012.20.34.png?alt=media&token=477fa169-8597-4330-a6d9-819cafd42fb8`,
+    stack: [
+      "Next Js",
+      "TypeScript",
+      "Astro",
+      "Next Auth",
+      "JWT",
+      "Prisma",
+      "Sass",
+    ],
+    github: "https://github.com/iairkap/linkforlife",
+  },
+  {
+    name: "BeyondTik",
+    backgroundContainer: "#0F121A",
+    picture:
+      "https://firebasestorage.googleapis.com/v0/b/real-cover.appspot.com/o/Screenshot%202024-02-15%20at%2011.46.15.png?alt=media&token=f0acf89d-c886-44ed-abd2-f3af08fa81e0",
+    stack: ["Astro", "TypeScript", "i18n"],
+
+    textExplainEn: `I had the opportunity to develop the website for BeyondTik, a versatile company specializing in Web Development, UX/UI Design, and Digital Marketing.
+
+    Utilizing ASTRO and TypeScript, I engineered a high-speed landing page tailored to BeyondTik's needs. ASTRO's speed optimization ensured a smooth user experience, perfectly aligning with BeyondTik's dedication to excellence.
+    
+    This bilingual landing page, available in both English and Hebrew, features a seamless Contact Form for effective communication. From development to design, including the creation of promotional videos, every aspect of this project was meticulously executed.`,
+    textExplainEs: `Tuve la oportunidad de desarrollar el sitio web para BeyondTik, una empresa versátil especializada en Desarrollo Web, Diseño UX/UI y Marketing Digital.
+    
+    Utilizando ASTRO y TypeScript, diseñé una página de aterrizaje de alta velocidad adaptada a las necesidades de BeyondTik. La optimización de velocidad de ASTRO garantizó una experiencia de usuario fluida, alineándose perfectamente con la dedicación de BeyondTik a la excelencia.
+    
+    Esta página de aterrizaje bilingüe, disponible en inglés y hebreo, cuenta con un formulario de contacto sin fisuras para una comunicación efectiva. Desde el desarrollo hasta el diseño, incluida la creación de videos promocionales, cada aspecto de este proyecto se ejecutó meticulosamente.`,
+    demo: "https://beyondtik.vercel.app",
+    github: "https://github.com/iairkap/marketing-israel-iair",
+  },
+
+  {
+    name: "Resilense",
+    textExplainEn: `Resilense, a pioneering company in its early stages, is reshaping the landscape of therapeutic support by providing comprehensive guidance from its inception. Specializing in advisory and support services, Resilense aims to offer innovative therapeutic resources to support emotional well-being. The platform, developed with leading technologies like Next.js and React, ensures a seamless, secure, and user-centered experience.
+
+    Our services encompass a variety of therapeutic resources, from puppets to video courses in the style of Udemy, available through subscription. Additionally, we provide a book for shared reading with children, strengthening emotional bonds between parents and children.
+    
+    As a technologically advanced company, we have integrated Video-Api for delivering captivating content and facilitated PayPal Integration for hassle-free transactions.
+    
+    At the core of Resilense's mission is the commitment to providing intuitive and secure therapeutic services, where each resource becomes a personalized expression of emotional support. With a user-friendly dashboard, data analytics, and strategic marketing insights, Resilense stands as a beacon for those seeking to enhance their emotional well-being.
+    
+    Explore the journey of resilience by visiting our website at resilense.com.
+    
+    For more details about our code, feel free to explore our GitHub repository.`,
+    backgroundContainer: "#5D16CC",
+    picture:
+      "https://firebasestorage.googleapis.com/v0/b/real-cover.appspot.com/o/iair%2FResilense.png?alt=media&token=d99b1f74-5f9e-4daa-9202-2dd388d38110",
+    stack: [
+      "Next Js",
+      "Prisma",
+      "I18n",
+      "Api Video",
+      "Paypal",
+      "Next-Auth",
+      "Zustand",
+    ],
+    textExplainEs: `Resilense, una empresa pionera en sus primeras etapas, está remodelando el panorama del apoyo terapéutico al proporcionar orientación integral desde su inicio. Especializada en servicios de asesoramiento y apoyo, Resilense tiene como objetivo ofrecer recursos terapéuticos innovadores para apoyar el bienestar emocional. La plataforma, desarrollada con tecnologías líderes como Next.js y React, garantiza una experiencia fluida, segura y centrada en el usuario.
+    
+    Nuestros servicios abarcan una variedad de recursos terapéuticos, desde títeres hasta cursos en video al estilo de Udemy, disponibles mediante suscripción. Además, proporcionamos un libro para lectura compartida con los niños, fortaleciendo los vínculos emocionales entre padres e hijos.
+
+    Como empresa tecnológicamente avanzada, hemos integrado Video-Api para ofrecer contenido cautivador y facilitado la Integración de PayPal para transacciones sin complicaciones.
+
+    En el núcleo de la misión de Resilense está el compromiso de proporcionar servicios terapéuticos intuitivos y seguros, donde cada recurso se convierte en una expresión personalizada de apoyo emocional. Con un panel de control fácil de usar, análisis de datos y conocimientos estratégicos de marketing, Resilense se erige como un faro para aquellos que buscan mejorar su bienestar emocional.
+    `,
+    demo: "https://resilense.com",
+  },
+  {
+    name: "Masa Programs App",
+    textExplainEn: `An innovative mobile application designed to cater to various mass groups, providing a comprehensive solution for pre-trip planning, in-Israel activities, and administrative management.
+
+    For Participants:
+    
+    - Display an interactive gallery showcasing detailed descriptions of available programs, including dates, duration, and highlighted activities.
+    
+    - Streamline the registration process, enabling participants to upload necessary documents, make secure payments, and receive automatic confirmations. Integration with Salesforce credentials ensures a seamless experience
+    
+    - Allow participants to view and customize their schedules and register for specific activities.
+    
+    - Provide a database of participating companies and search tools for participants to find suitable internship opportunities.
+    
+    - Provide a detailed daily calendar of activities with maps and directions to locations.
+    
+    - Enable participants to rate both activities and individuals, recording everything in the database for a comprehensive record of the best activities. LinkedIn credentials for completed activities.
+    
+    - Provide an administrator control panel for supervision and management.
+    
+    - Visualize various graphs to understand participant statistics for informed digital marketing decisions.
+    
+    - Integrate supplier orders into the app, automating processes like automatic supply orders based on uploaded information.
+    
+    Email Marketing:
+    
+    - Generate automatic emails based on weekly activities for parents, establishing an open communication channel.
+    
+    - Generate automatic emails for siblings of participants who are of age to register`,
+    picture:
+      "https://firebasestorage.googleapis.com/v0/b/real-cover.appspot.com/o/iair%2Fjavaia.png?alt=media&token=3b17b677-58a0-45e0-a638-8c3971c0421c",
+    stack: ["Next Js", "Prisma"],
+    backgroundContainer: "#091025",
+    demo: "https://javaia.vercel.app/",
+    textExplainEs: `
+    Diseñé una experiencia de usuario (UX/UI) receptiva para una aplicación integral utilizando Next.js. Integré características amigables para el usuario, como navegación intuitiva, registro sencillo y cargas seguras. El código desarrollado soporta la personalización de participantes, comunicación y compartición de contenido. Además, el panel de administración facilita una gestión eficiente del programa. Nuestro equipo interno de desarrollo garantiza una experiencia de usuario pulida.`,
+  },
+  {
     name: "Talent Tech Hub",
     picture:
-      "https://firebasestorage.googleapis.com/v0/b/real-cover.appspot.com/o/talent-tech-hub.png?alt=media&token=b3663675-b6f8-4047-b523-bf13c04acd06",
+      "https://firebasestorage.googleapis.com/v0/b/real-cover.appspot.com/o/iair%2FTalentTech.jpeg?alt=media&token=95aa054b-6b04-4fdd-aee4-ff9d00c3fb1d",
     textExplainEn: `At Soy Henry's bootcamp, I spearheaded a team to build a platform connecting Israeli startups with Latin American talent, addressing a global need. I fostered a collaborative environment, coordinated tasks, and kept our vision intact. We used various technologies like Next.js, Prisma, Vercel, and others for efficient development. The project was a testament to our skills, teamwork, and embodies Soy Henry's ethos of continuous learning and growth.`,
     stack: ["Next Js", "Prisma"],
     backgroundContainer: "#3D3272",
@@ -19,7 +143,14 @@ export const proyectosWebs = [
     picture: realcoverb,
     textExplainEs:
       "Real Cover es mi primer proyecto freelance, una tienda electrónica especializada en la venta al por mayor de fundas de notebooks personalizadas. Esta plataforma se ha diseñado para ofrecer una amplia gama de diseños únicos y personalizables que se adaptan a diversas preferencias y estilos. Gracias a la integración de Next.js, un marco de trabajo de React, se garantiza una experiencia de usuario optimizada, rápida y segura. Además, la implementación de Prisma, un ORM moderno para Node.js y TypeScript, permite un manejo eficiente y seguro de la base de datos. En conjunto, estas tecnologías permiten que Real Cover sea un servicio intuitivo, fácil de usar y completamente seguro para los clientes y para la gestión de su inventario. Con Real Cover, cada funda de notebook es más que una simple protección, es una expresión de identidad.",
-    stack: ["Next Js", "Prisma"],
+    stack: [
+      "Next Js",
+      "Prisma",
+      "Firebase",
+      "Cloudinary",
+      "Mercado Pago API",
+      "React Spring",
+    ],
     backgroundContainer: "#327234",
     demo: null,
     github: "https://github.com/iairkap/realcover",
